4f8074611dcfd5218a01ab1fa2ae5f01496589c6
Давиденко Александр Арсеньевич <davaa@plant.elesy.com>
2025-03-24 09:25:26 +0700

    new reg.js

--- reg.js
+++ reg.js
@@ -1,38 +1,62 @@
-const URL = 'https://devsanya.ru/api'
-document.getElementById('registerForm').addEventListener('submit', async (e) => {
+let rotateArrow = document.querySelector(".svg");
+const rotateArrowSupport = document.querySelector(".svg-support");
+
+const menuBlockProduct = document.querySelector(".menu__block-product");
+const menuBlockSupports = document.querySelector(".menu__block-supports");
+
+const productMobileList = document.querySelector(".product__mobile-list");
+const supportsMobileList = document.querySelector(".supports__mobile-list");
+
+menuBlockProduct.addEventListener("click", () => openMobileListComponents(productMobileList, rotateArrow));
+menuBlockSupports.addEventListener("click", () => openMobileListComponents(supportsMobileList, rotateArrowSupport));
+
+function openMobileListComponents(handlerClick, iconAnimate) {
+    if(handlerClick.classList.contains('mobile-list')) {
+        handlerClick.classList.remove('mobile-list');
+        handlerClick.classList.add('mobile-list-view');
+        iconAnimate.classList.add("rotate-arrow");
+    } else {
+        handlerClick.classList.remove('mobile-list-view');
+        iconAnimate.classList.remove("rotate-arrow");
+        handlerClick.classList.add('mobile-list');
+    }
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const formData = new FormData(e.target);
-    const userData = {
+    const formData = new FormData(form);
+    const data = {
+        fio: formData.get('fio'),
         email: formData.get('email'),
-        full_name: formData.get('full_name'),
         password: formData.get('password'),
         position: formData.get('position'),
         company: formData.get('company'),
-        industry: formData.get('industry'),
+        fieldActivity: formData.get('fieldActivity'),
         city: formData.get('city'),
-        phone: formData.get('phone'),
+        tel: formData.get('tel'),
     };
-
+    console.log(formData)
     try {
-        const response = await fetch(`${URL}/auth/register`, {
+        const response = await fetch('http://localhost:3000/register', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify(userData),
+            body: JSON.stringify(data)
         });
 
         const result = await response.json();
-
         if (response.ok) {
-            alert('Регистрация прошла успешно!');
-            window.location.href = '/login.html';
+            console.log(formData)
+            alert('Регистрация успешна');
+            window.location.href = 'auth.html';
         } else {
-            alert(`Ошибка: ${result.error}`);
+            alert(result.error);
         }
-    } catch (error) {
-        console.error('Ошибка при регистрации:', error);
-        alert('Произошла ошибка при регистрации. Попробуйте позже.');
+    } catch (err) {
+        alert('Ошибка при регистрации');
+        console.log(formData)
     }
 });
